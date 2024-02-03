f10bcd4e52361c08081e05e67875739945a8f674
Gavin Morgan <morgan.gavin@gmail.com>
2024-02-03 12:46:30 -0800

    some test data

--- rag.ts
+++ rag.ts
@@ -1,4 +1,14 @@
+import fs from "node:fs/promises";
 import { Document, VectorStoreIndex, QdrantVectorStore, Ollama, serviceContextFromDefaults } from "llamaindex";
+
+// const path = "node_modules/llamaindex/examples/abramov.txt";
+// const essay = await fs.readFile(path, "utf-8");
+// async function main() {
+//   await ingest({body: essay})
+//   await query()
+// }
+
+// main().catch(console.error);
 
 export async function ingest(event){
   const vectorStore = new QdrantVectorStore({
@@ -21,9 +31,8 @@ async function query() {
   const queryEngine = index.asQueryEngine();
 
   const response = await queryEngine.query({
-    query: "What did the author do in college?",
+    query: `What news is pertinent to my goal of ${status}?`,
   });
 
-  // Output response
   console.log(response.toString());
 }
